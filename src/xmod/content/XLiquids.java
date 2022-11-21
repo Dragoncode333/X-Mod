699bc7e0d8113df906513553ad266ebf985c27ee
Dragoncode333 <103138581+Dragoncode333@users.noreply.github.com>
2022-11-21 15:48:26 +0100

    Update XLiquids.java

--- src/xmod/content/XLiquids.java
+++ src/xmod/content/XLiquids.java
@@ -5,10 +5,17 @@ import mindustry.type.*;
 
 public class XLiquids{
     public static Liquid
+    aller;
     
 
     public static void load(){
-      
+        aller = new Liquid("aller", Color.valueOf("596ab8")){{
+            heatCapacity = 0.4f;
+            effect = StatusEffects.wet;
+            boilPoint = 0.5f;
+            gasColor = Color.grays(0.9f);
+            alwaysUnlocked = true;
+        }};
       
     }
 }
