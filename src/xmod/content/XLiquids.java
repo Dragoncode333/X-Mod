98eb0a47ed4c3fc7153ff22c15bcfc2affcbc08f
Dragoncode333 <103138581+Dragoncode333@users.noreply.github.com>
2022-11-21 15:42:50 +0100

    Create XLiquids.java

---
+++ src/xmod/content/XLiquids.java
@@ -0,0 +1,14 @@
+package xmod.content;
+
+import arc.graphics.*;
+import mindustry.type.*;
+
+public class XLiquids{
+    public static Liquid
+    
+
+    public static void load(){
+      
+      
+    }
+}
