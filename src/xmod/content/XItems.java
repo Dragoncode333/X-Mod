53caf3df17b3039b04fbb4e80b85447cb71220e7
Dragoncode333 <103138581+Dragoncode333@users.noreply.github.com>
2022-11-21 15:40:42 +0100

    Create XItems.java

---
+++ src/xmod/content/XItems.java
@@ -0,0 +1,17 @@
+package xmod.content;
+
+import arc.graphics.*;
+import arc.struct.*;
+import mindustry.type.*;
+
+public class Items{
+    public static Item
+    flydradiumRocket;
+    
+  public static void load(){
+        flydradiumRocket = new Item("flydradium-rocket", Color.valueOf("d99d73")){{
+        }};
+    
+  }
+  
+}
