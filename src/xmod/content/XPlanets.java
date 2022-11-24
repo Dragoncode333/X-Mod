e8424b2caaf0fe137c0ca0361c2a99a055c9459f
Dragoncode333 <103138581+Dragoncode333@users.noreply.github.com>
2022-11-24 11:00:01 +0100

    Update XPlanets.java

--- src/xmod/content/XPlanets.java
+++ src/xmod/content/XPlanets.java
@@ -47,8 +47,6 @@ public class XPlanets{
                 new HexSkyMesh(this, 11, 0.15f, 0.13f, 5, new Color().set(Pal.spore).mul(0.9f).a(0.75f), 2, 0.45f, 0.9f, 0.38f),
                 new HexSkyMesh(this, 1, 0.6f, 0.16f, 5, Color.white.cpy().lerp(Pal.spore, 0.55f).a(0.75f), 2, 0.45f, 1f, 0.41f)
             );
-
-            orbitRadius = 25f;
             
             launchCapacityMultiplier = 0.5f;
             sectorSeed = 2;
